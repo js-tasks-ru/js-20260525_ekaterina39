5a83ee4111d75350a69acedad941cd2d9ac9f898
Ekaterina Vostrikova <ekavos@hotmail.com>
2026-06-08 12:52:20 +0200

    task2/pick: done

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const allObjFields = Object.entries(obj);
+  const fieldsToPick = allObjFields.filter((f) => fields.includes(f[0]));
+  return Object.fromEntries(fieldsToPick);
 };
